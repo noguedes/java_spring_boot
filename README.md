ed19e58d96e69ea66a3de8239cf76d70a33ebfb8
noguedes <guedesbarroco@gmail.com>
2023-06-27 16:22:30 -0300

    Alterações adicionadas no README

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Acessar projeto no navegador: "localhost:8080"
 # Diário
 
 - 23/06/23: Criação de controller Hello World
+
+- 27/06/23: Criação de classe MedicoController 
--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("medicos")
+@RestController /*Esta anotação indica que esta classe deve ser carregada durante a inicialização do projeto */
+@RequestMapping("medicos") /*Esta anotação informa ao Spring que esta classe está mapeada na URL /medicos  */
 public class MedicoController {
 
-    @PostMapping
-    public void cadastrar(@RequestBody String json1) {
+    @PostMapping /*Informa o protocolo HTTP que este método ira lidar (no caso POST) */
+    public void cadastrar(@RequestBody /*Esta anotação informa ao Spring que o parametro ==> String json1 deve capiturar o body da requisição */ String json1) {
         System.out.println(json1);
     }
     
