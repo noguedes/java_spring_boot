18ca2ce776aff724e95eab3a561ed69786907435
noguedes <guedesbarroco@gmail.com>
2023-06-28 19:29:07 -0300

    erro (create record) função não encontrada, necessário atualizar o java

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,10 @@
 package med.voll.api.controller;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping /*Informa o protocolo HTTP que este método ira lidar (no caso POST) */
-    public void cadastrar(@RequestBody /*Esta anotação informa ao Spring que o parametro ==> String json1 deve capiturar o body da requisição */ String json1) {
-        System.out.println(json1);
+    public void cadastrar(@RequestBody /*Esta anotação informa ao Spring que o parametro ==> String json1 deve capiturar o body da requisição */ DadosCadastroMedico dados) {
+        System.out.println(dados);
     }
     
 }
